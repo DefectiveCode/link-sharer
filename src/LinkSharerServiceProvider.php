ca04f5e72f6048b0cce657739d11fc757e6db8d8
Corey McCormick <SimplyCorey@users.noreply.github.com>
2023-08-26 17:36:12 -0400

    Clean up

--- src/LinkSharerServiceProvider.php
+++ src/LinkSharerServiceProvider.php
@@ -3,7 +3,6 @@
 namespace DefectiveCode\LinkSharer;
 
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class LinkSharerServiceProvider extends ServiceProvider
@@ -11,17 +10,5 @@ class LinkSharerServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::component('link-sharer', LinkSharer::class);
-
-        Route::get('test', function () {
-            $html = <<<'HTML'
-                <script src="https://cdn.tailwindcss.com"></script>
-                <x-link-sharer service="viber" text="SHARE ME" url="https://google.com" class="p-4">
-                    <span class="text-red-900 font-bold m-10">SHARE ME</span>
-                </x-link-sharer>
-            HTML;
-
-            return Blade::render($html);
-        });
-
     }
 }
